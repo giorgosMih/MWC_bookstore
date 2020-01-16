ea53d55bb4b3312e7a659d0549de46caf8bbc85a
zefmarku <58485860+zefmarku@users.noreply.github.com>
2020-01-16 18:28:40 +0200

    Update login.php

--- internal/login.php
+++ internal/login.php
@@ -1,3 +1,32 @@
+<html>
+   
+   <head>
+      <title>Login Page</title>
+      
+      <style type = "text/css">
+         body {
+            font-family:Arial, Helvetica, sans-serif;
+            font-size:14px;
+         }
+         label {
+            font-weight:bold;
+            width:100px;
+            font-size:14px;
+         }
+         .box {
+            border:#666666 solid 1px;
+         }
+      </style>
+      
+   </head>
+   
+   <body bgcolor = "#FFFFFF">
+	
+      <div align = "center">
+         <div style = "width:300px; border: solid 1px #333333; " align = "left">
+            <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
+				
+            <div style = "margin:30px">
 <form method="post" action="index.php">
 Username: <input name="username"/>
 <br/>
@@ -6,3 +35,13 @@ Password: <input name="pass" type="password"/>
 <input type="submit" value="LOGIN"/>
 <input name="p" value="do_login" type="hidden">
 </form>
+
+					
+                    </div>
+                        
+                 </div>
+                    
+              </div>
+        
+           </body>
+        </html>
